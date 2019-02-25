8890c74748f4082b9ca6a14f4557a86fd04aa4a0
Einar Jonsson <einarjonsson@mac.com>
2019-02-25 11:40:05 +0100

    finished the questions and answer rails app with styling

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @message = params[:question]
+    if @message == nil
+      @message = ''
+      @answer = ''
+    elsif @message == 'I am going to work'
+      @answer = 'Great!'
+    elsif @message[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
+  get 'answer/:question', to: 'questions#answer', as: :question
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
